1b76a9e1aa16261872e10b547b0a69b723644f2e
Pavani Chowdary <boddu.pavani@research.iiit.ac.in>
2021-07-24 23:25:53 +0530

    updated test

--- Code/bot.py
+++ Code/bot.py
@@ -31,26 +31,27 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
     
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    harry_potter = [
-        'I hope you\'re pleased with yourselves. We could all have been killed — or worse, expelled. Now if you don\'t mind, I\'m going to bed.',
-        'Yer a wizard Harry.','When in doubt, go to the library. ','Dobby is free', 'Honestly, if you were any slower, you’d be going backward'
-        ,'I’ll be in my bedroom, making no noise and pretending I’m not there.','Mischief Managed!','Twitchy little ferret, aren’t you, Malfoy?',
-        'I mean, it\'s sort of exciting, isn\'t it, breaking the rules?','I am a wizard, not a baboon brandishing a stick.'
-
-    ]
     
-    if message.content == 'test':
-        response = random.choice(harry_potter)
-        await message.channel.send(response)
+#test
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
+#     harry_potter = [
+#         'I hope you\'re pleased with yourselves. We could all have been killed — or worse, expelled. Now if you don\'t mind, I\'m going to bed.',
+#         'Yer a wizard Harry.','When in doubt, go to the library. ','Dobby is free', 'Honestly, if you were any slower, you’d be going backward'
+#         ,'I’ll be in my bedroom, making no noise and pretending I’m not there.','Mischief Managed!','Twitchy little ferret, aren’t you, Malfoy?',
+#         'I mean, it\'s sort of exciting, isn\'t it, breaking the rules?','I am a wizard, not a baboon brandishing a stick.'
+
+#     ]
+    
+#     if message.content == 'test':
+#         response = random.choice(harry_potter)
+#         await message.channel.send(response)
+
+#     elif message.content == 'raise-exception':
+#         raise discord.DiscordException
         
     
     
